205140b4281872e56173cbdf54c5d72b23607d62
jeroboby <jerome.mmetge@gmail.com>
2018-12-04 16:37:31 +0100

    first commit RecipeBook Udemy

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,58 @@
+import { Subject } from 'rxjs';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Recipe } from "./recipe.model";
+import { Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
+@Injectable()
+export class RecipeService {
+    recipeChanged = new Subject<Recipe[]>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Tasty Schnitzel', 
+            'A super-tasty Schnitzel - just awesome !', 
+            'https://previews.123rf.com/images/foodandmore/foodandmore1502/foodandmore150200150/36578073-close-up-tasty-recipe-of-crumbled-escalope-with-potato-fries-styled-with-slice-of-lemon-and-tomato-a.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French fries', 20)
+            ]),
+        new Recipe(
+            'Big fat burger', 
+            'What else you need to say ?', 
+            'https://st2.depositphotos.com/1004118/11101/i/950/depositphotos_111015972-stock-photo-big-fat-burger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
+      ];
+
+      constructor(private shoppingListService: ShoppingListService) {}
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
+
+      getRecipe(index: number) {
+          return this.recipes[index];
+      }
+
+      addingredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+      }
+
+      addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipeChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipeChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number) {
+          this.recipes.splice(index, 1);
+          this.recipeChanged.next(this.recipes.slice());
+      }
+}
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, OnInit, HostListener, HostBinding } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen: boolean = false;
+
+    @HostListener('click') toggleOpen(eventData: Event) {
+        this.isOpen = !this.isOpen;
+    }
+
+}
